1efa07587c0acc8cb8b7355fc4275d97d783cc12
nerox118 <arulrank2@gmail.com>
2021-04-04 16:47:38 +0700

    hapus

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "lokania";
-$db = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
-$conn = new mysqli($host, $username, $password, $database);
-?>
